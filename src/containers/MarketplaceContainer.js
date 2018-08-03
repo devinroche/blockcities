d402fe693a360e26ed055932590d6e0110ac91c1
Devin Roche <Roche.devin@ymail.com>
2018-08-03 12:36:30 -0700

    remove unused import

--- src/containers/MarketplaceContainer.js
+++ src/containers/MarketplaceContainer.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
 import Navbar from './NavContainer';
 import Footer from './FooterContainer';
 import styles from '../theme/styles/Containers.style';
 import Marketplace from '../components/Marketplace';
 
-const MarketplaceContainer = props => (
-    <Grid style={styles.grid}>
-        <Navbar navigation={props.navigation} />
-        <Row size={75}>
-            <Col size={5} />
-            <Col size={90} >
-                <Marketplace />
-            </Col>
-            <Col size={5} />
-        </Row>
-        <Footer navigation={props.navigation} />
-    </Grid>
-);
+const MarketplaceContainer = (props) => {
+    const { navigation } = props;
+    return (
+        <Grid style={styles.grid}>
+            <Navbar navigation={navigation} />
+            <Row size={75}>
+                <Col size={5} />
+                <Col size={90}>
+                    <Marketplace />
+                </Col>
+                <Col size={5} />
+            </Row>
+            <Footer navigation={navigation} />
+        </Grid>
+    );
+};
 
 const mapStateToProps = state => (state.reducer);
 
